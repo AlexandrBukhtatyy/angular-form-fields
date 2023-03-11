41ce4878367ed183d5f17371e7a529ddec683047
a.bukhtatiy <buhtatyalexander90@gmail.com>
2023-03-11 22:31:03 +0300

    fix stackblitz bugs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import 'zone.js'; // Need for working in stackblitz
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
