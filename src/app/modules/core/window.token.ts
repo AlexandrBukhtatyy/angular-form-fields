3723fdd993f7557d7ce7a472ec0d19d3da0c2648
a.bukhtatiy <buhtatyalexander90@gmail.com>
2023-03-11 22:21:55 +0300

    remove code

--- src/app/modules/core/window.token.ts
+++
@@ -1,9 +0,0 @@
-import {inject, InjectionToken} from "@angular/core";
-import {DOCUMENT} from "@angular/common";
-
-export const WINDOW = new InjectionToken<Window>(
-  'An abstraction over global window object',
-  {
-    factory: () => inject(DOCUMENT).defaultView!
-  },
-);
