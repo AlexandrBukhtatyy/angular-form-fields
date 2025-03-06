7da4e54cf329c1c9177c2e2b9468c0b53e7b3b9f
a.bukhtatiy <buhtatyalexander90@gmail.com>
2025-03-06 20:27:19 +0300

    make core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
-import {routes} from './config/routes';
 import {TUI_SANITIZER} from '@taiga-ui/legacy';
 import {NG_EVENT_PLUGINS} from '@tinkoff/ng-event-plugins';
 import {AuthModule} from '@modules/auth';
-import {AuthService} from './core/services/auth.service';
+import {AuthService} from '@core/services/auth.service';
+import {routes} from '@core/config/routes';
 
 
 @NgModule({
