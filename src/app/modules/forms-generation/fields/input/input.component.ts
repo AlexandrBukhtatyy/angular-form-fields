4d946f8d1fa7583530047ed7603f5545d8147370
a.bukhtatiy <buhtatyalexander90@gmail.com>
2025-03-14 23:31:52 +0300

    use own form-field in form generation

--- src/app/modules/forms-generation/fields/input/input.component.ts
+++ src/app/modules/forms-generation/fields/input/input.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FieldType, FieldTypeConfig} from '@ngx-formly/core';
+import {InputComponent as FInputComponent} from '@modules/forms';
 
 interface InputProps {
   type: 'input';
+  label: string;
 }
 
 @Component({
-    selector: 'fgen-input',
-    imports: [],
-    templateUrl: './input.component.html',
-    styleUrl: './input.component.less'
+  selector: 'fgen-input',
+  imports: [
+    FInputComponent
+  ],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.less',
+  standalone: true
 })
 export class InputComponent extends FieldType<FieldTypeConfig<InputProps>> {
   get type() {
