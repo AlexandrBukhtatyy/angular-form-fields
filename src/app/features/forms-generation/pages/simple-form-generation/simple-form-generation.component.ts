5670a8909e105de43a18af78494bebcb9013bb8d
a.bukhtatiy <buhtatyalexander90@gmail.com>
2025-03-16 10:36:54 +0300

    markup simple generated form

--- src/app/features/forms-generation/pages/simple-form-generation/simple-form-generation.component.ts
+++ src/app/features/forms-generation/pages/simple-form-generation/simple-form-generation.component.ts
@@ -5,28 +5,98 @@ import {FormlyFieldConfig, FormlyModule} from '@ngx-formly/core';
 import {TuiButton} from '@taiga-ui/core';
 
 @Component({
-    selector: 'aff-simple-form-generation',
-    imports: [
-        ReactiveFormsModule,
-        FormsGenerationModule,
-        FormlyModule,
-        TuiButton,
-    ],
-    templateUrl: './simple-form-generation.component.html',
-    styleUrl: './simple-form-generation.component.less'
+  selector: 'aff-simple-form-generation',
+  imports: [
+    ReactiveFormsModule,
+    FormsGenerationModule,
+    FormlyModule,
+    TuiButton,
+  ],
+  templateUrl: './simple-form-generation.component.html',
+  styleUrl: './simple-form-generation.component.less',
+  standalone: true
 })
 export class SimpleFormGenerationComponent {
   form = new FormGroup({});
   model = {email: 'email@gmail.com'};
   fields: FormlyFieldConfig[] = [
     {
-      key: 'email',
-      type: 'input',
-      props: {
-        label: 'Email address',
-        placeholder: 'Enter email',
-        required: true,
-      }
+      className: 'tui-form__row',
+      fieldGroupClassName: 'tui-form__row_multi-fields',
+      fieldGroup: [
+        {
+          key: 'address',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Address',
+            placeholder: 'Address',
+            required: true,
+          }
+        },
+        {
+          key: 'entrance',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Entrance',
+            placeholder: 'Entrance',
+            required: true,
+          }
+        },
+      ]
+    },
+    {
+      className: 'tui-form__row',
+      fieldGroupClassName: 'tui-form__row_multi-fields',
+      fieldGroup: [
+        {
+          key: 'floor',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Floor',
+            placeholder: 'Floor',
+            required: true,
+          }
+        },
+        {
+          key: 'appartament',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Email Appartament',
+            placeholder: 'Appartament',
+            required: true,
+          }
+        },
+        {
+          key: 'code',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Code',
+            placeholder: 'Code',
+            required: true,
+          }
+        },
+      ]
+    },
+    {
+      className: 'tui-form__row',
+      fieldGroupClassName: 'tui-form__row_multi-fields',
+      fieldGroup: [
+        {
+          key: 'comment',
+          type: 'input',
+          className: 'tui-form__multi-field',
+          props: {
+            label: 'Comment',
+            placeholder: 'Comment',
+            required: true,
+          }
+        },
+      ]
     }
   ];
 
